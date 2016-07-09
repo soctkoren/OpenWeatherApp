26793c13e73a68b895f6e96467c779595521207c
John Kim <joimkim@gmail.com>
2016-07-08 21:35:03 -0700

    api and geolocation integrated

--- index_ctrl.js
+++ index_ctrl.js
@@ -1,8 +1,28 @@
-var IndexCtrl = app.controller('IndexCtrl', [function() {
+var IndexCtrl = app.controller('IndexCtrl', ['$http', '$q', function($http, $q) {
+    var API_KEY = '8f6c3116d20a6fa29db7e883b0f7e179';
 
-this.input = 'blah';
+    var lat;
+    var lon;
 
-this.test = function() {
-  console.log('it is working');
-}
+    //get lats and lons
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log('lo');
+      lat = position.coords.latitude;
+      lon = position.coords.longitude;
+      this.apiCall();
+    }.bind(this));
+
+    //get weather API call
+    this.apiCall = function() {
+      $http.get('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&APPID='+API_KEY)
+        .then(function(response) {
+          console.log(response);
+      });
+    };
+
+  console.log("yo");
+
+  this.test = function() {
+    console.log('it is working');
+  };
 }]);
