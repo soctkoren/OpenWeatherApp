64df9d24bd7734fdabdcc18dd7abae9cff1a497e
John Kim <joimkim@gmail.com>
2016-07-08 23:34:37 -0700

    temp toggle, weather iconimplemented

--- index_ctrl.js
+++ index_ctrl.js
@@ -3,6 +3,18 @@ var IndexCtrl = app.controller('IndexCtrl', ['$http', '$q', function($http, $q) 
 
     var lat;
     var lon;
+    var obj;
+    this.weatherObj = {
+      humidity: '',
+      temp: '',
+      tempCelsius: '',
+      tempFahrenheit: '',
+      name: '',
+      description: '',
+      icon: ''
+    };
+
+    this.isCelsius = true;
 
     //get lats and lons
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -17,12 +29,33 @@ var IndexCtrl = app.controller('IndexCtrl', ['$http', '$q', function($http, $q) 
       $http.get('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&APPID='+API_KEY)
         .then(function(response) {
           console.log(response);
-      });
+          obj = response;
+          this.weatherObj.humidity = obj.data.main.humidity;
+          this.weatherObj.tempCelsius = kelvinToCelsius(obj.data.main.temp);
+          this.weatherObj.tempFahrenheit = celsiusToFahrenheit(this.weatherObj.tempCelsius);
+          this.weatherObj.temp = this.weatherObj.tempCelsius;
+          this.weatherObj.name = obj.data.name;
+          this.weatherObj.description = obj.data.weather[0].description;
+          this.weatherObj.icon = obj.data.weather[0].icon;
+      }.bind(this));
     };
 
-  console.log("yo");
-
-  this.test = function() {
-    console.log('it is working');
+  var celsiusToFahrenheit = function(temp) {
+    return Math.round(temp * 1.8 + 32);
   };
+
+  var kelvinToCelsius = function(temp) {
+    return Math.round(temp - 273.15);
+  };
+
+  this.toggleTemp = function() {
+    if (this.isCelsius) {
+      this.weatherObj.temp = this.weatherObj.tempFahrenheit;
+    } else {
+      this.weatherObj.temp = this.weatherObj.tempCelsius;
+    }
+
+    this.isCelsius = !this.isCelsius;
+  };
+
 }]);
