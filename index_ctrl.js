ab866559865e7c6f287f6b4cbf1ec9fe7d1069ce
iamjoo <iamjoo@gmail.com>
2016-07-16 14:35:09 -0700

    successful integration of new api

--- index_ctrl.js
+++ index_ctrl.js
@@ -1,14 +1,12 @@
 var IndexCtrl = app.controller('IndexCtrl', ['$http', function($http) {
   /** @const {string} */
-  // const API_KEY = '8f6c3116d20a6fa29db7e883b0f7e179';
   const API_KEY = '659f89cf0dd4f5c254d169cafbc41e9f';
 
   /** @const {string} */
-  // const API_URL = 'http://api.openweathermap.org/data/2.5/weather?lat=';
   const API_URL = 'https://api.forecast.io/forecast/';
 
   /** @type {boolean} */
-  this.isCelsius = true;
+  this.isFahrenheit = true;
 
   /** @type {boolean} */
   this.isReady = false;
@@ -32,6 +30,11 @@ var IndexCtrl = app.controller('IndexCtrl', ['$http', function($http) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     this.apiCall_(latitude, longitude);
+  }.bind(this), function(error) {
+    // Default to Sunnyvale on error.
+    let latitude = 37.3688;
+    let longitude = -122.0363;
+    this.apiCall_(latitude, longitude);
   }.bind(this));
 
   /**
@@ -41,54 +44,44 @@ var IndexCtrl = app.controller('IndexCtrl', ['$http', function($http) {
    * @param {number} longitude
    */
   this.apiCall_ = function(latitude, longitude) {
-    // $http.get(API_URL + latitude + '&lon=' + longitude + '&APPID=' + API_KEY)
     $http.get(API_URL + API_KEY + '/' + latitude + ',' + longitude)
         .then(function(response) {
           console.log(response);
-          this.weatherObj.humidity = response.data.main.humidity;
-          this.weatherObj.tempCelsius =
-              kelvinToCelsius_(response.data.main.temp);
+          this.weatherObj.humidity = response.data.currently.humidity;
           this.weatherObj.tempFahrenheit =
-              celsiusToFahrenheit_(this.weatherObj.tempCelsius);
-          this.weatherObj.temp = this.weatherObj.tempCelsius;
-          this.weatherObj.name = response.data.name;
-          this.weatherObj.description = response.data.weather[0].description;
-          this.weatherObj.icon = response.data.weather[0].icon;
+              Math.round(response.data.currently.temperature);
+          this.weatherObj.tempCelsius =
+              fahrenheitToCelsius_(response.data.currently.temperature);
+          this.weatherObj.temp = this.weatherObj.tempFahrenheit;
+          this.weatherObj.name = response.data.timezone;
+          this.weatherObj.description = response.data.currently.summary;
+          this.weatherObj.icon = response.data.currently.icon;
 
+          console.log(this.weatherObj);
           this.isReady = true;
         }.bind(this));
   };
 
   /**
-   * Converts temperature in Celsius to Fahrenheit.
+   * Converts temperature in Fahrenheit to Celsius.
    * @private
    * @param {number} temp
    * @return {number}
    */
-  var celsiusToFahrenheit_ = function(temp) {
-    return Math.round(temp * 1.8 + 32);
-  };
-
-  /**
-   * Converts temperature in Kelvin to Celsius.
-   * @private
-   * @param {number} temp
-   * @return {number}
-   */
-  var kelvinToCelsius_ = function(temp) {
-    return Math.round(temp - 273.15);
+  var fahrenheitToCelsius_ = function(temp) {
+    return Math.round((temp - 32) / 1.8);
   };
 
   /**
    * Switches between displaying the temperature in Celsius and Fahrenheit.
    */
   this.toggleTemp = function() {
-    if (this.isCelsius) {
-      this.weatherObj.temp = this.weatherObj.tempFahrenheit;
-    } else {
+    if (this.isFahrenheit) {
       this.weatherObj.temp = this.weatherObj.tempCelsius;
+    } else {
+      this.weatherObj.temp = this.weatherObj.tempFahrenheit;
     }
 
-    this.isCelsius = !this.isCelsius;
+    this.isFahrenheit = !this.isFahrenheit;
   };
 }]);
